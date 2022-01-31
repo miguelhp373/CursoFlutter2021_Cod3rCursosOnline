6b135050985054c5ef020be6ea7db45e91f606ed
Miguel Henrique <miguelhp373@gmail.com>
2022-01-30 22:03:45 -0300

    Update 12 - Map_&_Reduce.dart

---
+++ Dart/12 - Map_&_Reduce.dart
@@ -0,0 +1,22 @@
+main() {
+  var alunos = [
+    {'nome': 'Alfredo', 'nota': 9.9},
+    {'nome': 'Wilson', 'nota': 8.7},
+    {'nome': 'Guilherme', 'nota': 9.3},
+    {'nome': 'Ana', 'nota': 6.8},
+    {'nome': 'Ricardo', 'nota': 8.1},
+    {'nome': 'Mariana', 'nota': 7.6},
+  ];
+
+  //map to string and integer
+
+  String Function(Map) getOnlyName = (aluno) => aluno['nome'];
+  int Function(String) getTextLenght = (texto) => texto.length;
+
+  var nomes = alunos.map(getOnlyName);
+  var textLength = alunos.map(getTextLenght);
+  print(nomes);
+  print(textLength);
+
+  ////////////////
+}
