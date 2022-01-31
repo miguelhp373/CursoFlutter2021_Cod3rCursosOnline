e09274e768fae1abb9133f8c3b3c517de11459ba
Miguel Henrique <miguelhp373@gmail.com>
2022-01-30 22:02:33 -0300

    add

---
+++ Dart/07 - Funcao_em_Variavel_Anonima.dart
@@ -0,0 +1,17 @@
+//FUNCÕES ANONIMAS
+
+main() {
+  var adicao = (int a, int b) {
+    return a + b;
+  };
+
+  print(adicao(4, 9));
+
+  var subtracao = (int a, int b) => a - b;
+
+  print(subtracao(5, 4));
+}
+
+//  -------------------------
+//13
+//1
