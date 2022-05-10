28f5650af5b69f3accb1983b939ed7463ecf3d05
Miguel Henrique <miguelhp373@gmail.com>
2022-05-09 23:49:27 -0300

    composição #2

---
+++ Dart/Orientação a Objetos/22.Composição/modelo/venda_item.dart
@@ -0,0 +1,22 @@
+import 'produto.dart';
+
+class VendaItem {
+  Produto produto;
+  int quantidade;
+  double _preco;
+
+  VendaItem({required this.produto, this.quantidade = 1, this._preco});
+
+  double get preco {
+    if (produto != null && _preco == null) {
+      _preco = produto.precoComDesconto;
+    }
+    return _preco;
+  }
+
+  void set preco(double novoPreco) {
+    if (novoPreco > 0) {
+      _preco = novoPreco;
+    }
+  }
+}
