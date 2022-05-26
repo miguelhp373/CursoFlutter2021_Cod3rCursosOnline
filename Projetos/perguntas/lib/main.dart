c46a38b6eeabfc80d3b9e8a8a36718764fc8e6c9
Miguel Henrique <miguelhp373@gmail.com>
2022-05-25 23:34:03 -0300

    Aulas 32 a 36 - Widgets

--- Projetos/perguntas/lib/main.dart
+++ Projetos/perguntas/lib/main.dart
@@ -7,10 +7,58 @@ main() {
 class PerguntasApp extends StatelessWidget {
   const PerguntasApp({Key? key}) : super(key: key);
 
+  void aswnerQuestion() {}
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('hello World!'),
+    double fixValueForMarginsTop = 10;
+
+    final List<String> questions = [
+      'Qual sua Cor Favorita?',
+      'Qual seu Animal Favorito?',
+    ];
+
+    final List<String> questionsAswer = [
+      'xxxxxxxxxxx',
+      'xxxxxxxxxxx',
+      'xxxxxxxxxxx',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(fixValueForMarginsTop),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(questions[0]),
+                  SizedBox(height: fixValueForMarginsTop),
+                  ElevatedButton(
+                    onPressed: () => {aswnerQuestion()},
+                    child: Text(questionsAswer[0]),
+                  ),
+                  SizedBox(height: fixValueForMarginsTop),
+                  ElevatedButton(
+                    onPressed: () => {},
+                    child: Text(questionsAswer[1]),
+                  ),
+                  SizedBox(height: fixValueForMarginsTop),
+                  ElevatedButton(
+                    onPressed: () => {},
+                    child: Text(questionsAswer[2]),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
