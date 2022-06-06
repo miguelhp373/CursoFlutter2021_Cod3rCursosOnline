616b95ce2a24abfcf6fdc72d7074ddd107fff570
Miguel Henrique <miguelhp373@gmail.com>
2022-06-06 00:46:22 -0300

    implementando componentização

---
+++ Projetos/perguntas/lib/global/app_definitions.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppDefinitions {
+  static double fixValueForMarginsTop = 10;
+
+  static Color colorPrimaryText = Colors.white;
+  static Color colorSecondaryButton = Colors.grey;
+  static Color colorContainerCard = Colors.lightBlue;
+}
--- Projetos/perguntas/lib/main.dart
+++ Projetos/perguntas/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:perguntas/components/aswner_button.dart';
-import 'package:perguntas/components/question_text.dart';
+import 'package:perguntas/views/aswners_layout.dart';
 
 main() {
   runApp(const PerguntasApp());
@@ -16,106 +15,48 @@ class PerguntasApp extends StatefulWidget {
 class _PerguntasAppState extends State<PerguntasApp> {
   int _questionSelected = 0;
 
+  //List<type<datatype, key>>
+  final List<Map<String, Object>> _questions = [
+    {
+      'question': 'Qual sua Cor Favorita?',
+      'aswner': ['Azul', 'Preto', 'Vermelho', 'Preto']
+    },
+    {
+      'question': 'Qual seu Animal Favorito?',
+      'aswner': ['Cachorro', 'Gato', 'Passáro', 'Coelho']
+    }
+  ];
+
   void _aswnerSelect() {
     setState(() {
-      if (_questionSelected < 1) {
-        _questionSelected = 1;
-      } else {
-        _questionSelected = 0;
-      }
+      _questionSelected++;
     });
+  }
+
+  bool get selectedQuestion {
+    return _questionSelected < _questions.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    //default const values on application
-    double fixValueForMarginsTop = 10;
-    //colors define
-    const Color colorPrimaryText = Colors.white;
-    const Color colorSecondaryButton = Colors.grey;
-    const Color colorContainerCard = Colors.lightBlue;
-    //List<type<datatype, key>>
-    final List<Map<String, Object>> questions = [
-      {
-        'question': 'Qual sua Cor Favorita?',
-        'aswner': ['Azul', 'Preto', 'Vermelho', 'Preto']
-      },
-      {
-        'question': 'Qual seu Animal Favorito?',
-        'aswner': ['Cachorro', 'Gato', 'Passáro', 'Coelho']
-      }
-    ];
-
-    List<String> awsnerList = questions[_questionSelected].cast()['aswner'];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              const Text('Perguntas'),
-            ],
+            children: [const Text('Perguntas')],
           ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 250,
-                  height: 300,
-                  margin: EdgeInsets.all(fixValueForMarginsTop),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: colorContainerCard,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset:
-                            const Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          QuestionText(
-                            questions[_questionSelected]['question'].toString(),
-                            colorPrimaryText,
-                          ),
-                          SizedBox(height: fixValueForMarginsTop),
-                          ...awsnerList
-                              .map(
-                                (displayTextonButton) => AswnerButton(
-                                  displayTextonButton,
-                                  _aswnerSelect,
-                                  ElevatedButton.styleFrom(
-                                    primary: colorSecondaryButton,
-                                    textStyle: const TextStyle(
-                                        color: colorPrimaryText),
-                                  ),
-                                  fixValueForMarginsTop,
-                                ),
-                              )
-                              .toList(), //spread
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+        body: selectedQuestion
+            ? AswnersLayout(
+                aswnerParam: _aswnerSelect,
+                questionsParam: _questions,
+                selectQuestionParam: _questionSelected,
+              )
+            : const Center(
+                child: Text('Parabéns!'),
+              ),
       ),
     );
   }
